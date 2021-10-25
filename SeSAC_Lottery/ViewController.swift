8faa5c31034b6a539e74e8eff36d05f5b18abaf7
ChanhoHwang <ChanhoHwang>
2021-10-25 18:17:31 +0900

    Initial Commit

---
+++ SeSAC_Lottery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC_Lottery
+//
+//  Created by ChanhoHwang on 2021/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
